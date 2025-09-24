48be8ba094de9df3dc54b3ddc4f9963f6ae8c574
José Manuel <68438454+Josemajr6@users.noreply.github.com>
2025-09-24 14:30:23 +0200

    Ejercicios 6, 7 y 8 del día 24/09/2025
    Lo he subido en dos tandas ya que GitHub no deja subir más de 100 archivos a la vez

---
+++ Ejercicios 24-09/Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Indica el tamaño del array: ");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            int[] numeros = new int[cantidad];
+
+            for (int i = 0; i <= cantidad - 1; i++)
+            {
+                Console.Write("Indica el valor " + (i+1) + " del array: ");
+                numeros[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("");
+            for (int i = 0; i <= cantidad -1; i++)
+            {
+                if (numeros[i] % 2 == 0)
+                {
+                    Console.WriteLine(numeros[i] + " es par");
+                }
+                else
+                {
+                    Console.WriteLine(numeros[i] + " es impar");
+                }
+            }
+
+        }
+    }
+}
