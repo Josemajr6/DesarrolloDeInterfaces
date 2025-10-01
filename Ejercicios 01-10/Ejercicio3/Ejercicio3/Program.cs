3fb772476b1379a54866f55d9ddd1328f93e7b74
José Manuel <68438454+Josemajr6@users.noreply.github.com>
2025-10-01 14:27:40 +0200

    Ejercicios 01/10 (Relación 3 de ejercicios Tema 1)

---
+++ Ejercicios 01-10/Ejercicio3/Ejercicio3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce un número y calcularé el factorial: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Factorial: " + Factorial(numero));
+
+        }
+
+        public static int Factorial(int numero)
+        {
+            if (numero == 0 || numero == 1)
+            {
+                return 1;
+            }
+
+            else
+            {
+                return numero * Factorial(numero - 1);
+            }
+        }
+    }
+
+
+
+
+    }
---
+++ Ejercicios 01-10/Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduce un número y te diré si es primo: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            bool primo = esPrimo(numero);
+
+            if (primo)
+            {
+                Console.WriteLine("El número " + numero + " es primo");
+            }
+            else
+            {
+                Console.WriteLine("El número " + numero + " no es primo");
+            }
+        }
+
+        public static bool esPrimo(int numero)
+        {
+            bool esPrimo = true;
+            for (int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    esPrimo = false;
+                }
+            }
+            return esPrimo;
+        }
+    }
+}
