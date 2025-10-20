7800e2837d3bf0882791700f2e3a0f9232bfad8f
José Manuel <68438454+Josemajr6@users.noreply.github.com>
2025-10-20 10:53:00 +0200

    Add files via upload

---
+++ Tema 3/Práctica 1/Ejercicio6/Ejercicio6/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Error_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Creo un try ejecutar el código que va a fallar
+            try
+            {
+                
+                // Creo un array con 3 índices que son 0, 1 y 2
+                int[] numeros = new int[3];
+
+                // Intento asignar al índice 5 un valor que esto va a dar error
+                numeros[5] = 10; 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: El índice está fuera de los límites del array" );
+            }
+
+
+
+        }
+    }
+}
