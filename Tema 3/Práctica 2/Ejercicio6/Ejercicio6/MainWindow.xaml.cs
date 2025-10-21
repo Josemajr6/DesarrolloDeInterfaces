05792176f6ecfba52af6b41113c82c44e509c7e3
José Manuel <josemajimenezrodriguez8@gmail.com>
2025-10-21 14:33:24 +0200

    Práctica 2 del Tema 3

---
+++ Tema 3/Práctica 2/Ejercicio6/Ejercicio6/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerde_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            MessageBox.Show("El contenido del botón es: " + btn.Content);
+        }
+    }
+}
---
+++ Tema 3/Práctica 2/Ejercicio7/Ejercicio7/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Boton_Click(object sender, MouseButtonEventArgs e)
+        {
+            string textoSeleccionado = tbTexto.SelectedText;
+
+            string mensaje = "Has seleccionado: " + textoSeleccionado;
+
+            MessageBox.Show(mensaje);
+        }
+    }
+}
