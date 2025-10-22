baa07bb27c0f39b3d335070b8045afb3272c3b0d
José Manuel <68438454+Josemajr6@users.noreply.github.com>
2025-10-22 14:41:58 +0200

    Add files via upload

---
+++ Tema 3/Práctica 3/Ejercicio 1/Ejercicio 1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public partial class MainWindow : Window
+    {
+
+        private int intentosRealizados = 0;
+        private const int MAX_INTENTOS = 2;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            tbContador.Text = "Te quedan " + MAX_INTENTOS + " intentos";
+        }
+
+        private void Corregir_Click(object sender, RoutedEventArgs e)
+        {
+          
+            if (rbCorrecta.IsChecked == true)
+            {
+                MessageBox.Show("Has acertado. Además de tener mejor calidad que las demás, están las tres en una sola APP");
+
+         
+                btnCorregir.IsEnabled = false;
+                panelRespuestas.IsEnabled = false;
+                tbContador.Text = "Te quedan " + MAX_INTENTOS + " intentos";
+            }
+            else
+            {
+               
+                intentosRealizados++; 
+                int intentosRestantes = MAX_INTENTOS - intentosRealizados;
+
+                if (intentosRestantes > 0)
+                {
+                   
+                    MessageBox.Show("Respuesta incorrecta. Vuelve a intentarlo");
+                   
+                    tbContador.Text = "Te quedan " + intentosRestantes + " intentos";
+                } 
+                else
+                {
+                   
+                    MessageBox.Show("Respuesta incorrecta. No tienes más intentos");
+                    tbContador.Text = "No tienes mas intentos";
+
+                  
+                    btnCorregir.IsEnabled = false;
+                    panelRespuestas.IsEnabled = false;
+                    rbCorrecta.IsChecked = true; 
+                }
+            }
+        }
+    }
+}
