7bae3902bd37c4dc55637f5cb256244612b43269
José Manuel <josemajimenezrodriguez8@gmail.com>
2025-11-03 11:04:26 +0100

    Subida de la práctica 6 del Tema 3 (Falta ejercicio 4)

---
+++ Tema 3/Práctica 6/Ejercicio 2/Ejercicio 2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /* 
+         Método de Cortar_CanExecute para que se el botón se pueda pulsar sólo si el
+         txtBox no está vacío y hay al menos un carácter seleccionado
+         */
+        private void Cortar_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = (txtEditor != null) && (txtEditor.SelectionLength > 0);
+        }
+
+        // Método para que corte desde el txtBox
+        private void Cortar_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtEditor.Cut();
+        }
+
+        // Método para que habilite el botón sólo si hay texto en el portapapeles
+        private void Pegar_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = Clipboard.ContainsText();
+        }
+
+        // Métdo para que se pegue en el txtBox el texto del portapapeles
+        private void Pegar_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            txtEditor.Paste();
+        }
+    }
+}
---
+++ Tema 3/Práctica 6/Ejercicio 3/Ejercicio 3/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnviar_Click(object sender, RoutedEventArgs e)
+        {
+            // Recogemos los datos de los txtBox y creamos el string con el mensaje y el título
+            string nombre = txtNombre.Text;
+            string email = txtCorreo.Text;
+            string mensaje = txtMensaje.Text;
+
+            string textoDelMensaje = "El usuario " + nombre + " con email " + email + " envía el mensaje: " + mensaje;
+            string titulo = "Datos del Formulario";
+
+            // Mostramos el MessageBox con título, texto, botón OK e icono de Información
+            MessageBox.Show(textoDelMensaje, titulo, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
