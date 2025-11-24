766f91698b4f544fd00dc05a7f915840a888c0d3
José Manuel <josemajimenezrodriguez8@gmail.com>
2025-11-24 11:01:43 +0100

    Subida de ejercicios 1 y 2 de la práctica 10

---
+++ Tema 3/Práctica 10/Ejercicio 1/Ejercicio 1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tbDNI_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string cadena = tbDNI.Text;
+            if (cadena.Length == 8)
+            {
+                int DNI = int.Parse(cadena);
+                int resto = DNI % 23;
+                char[] letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
+                string resultado = "La letra del DNI es la " + letras[resto];
+                tbResultado.Text = resultado;
+            }
+            else
+            {
+                tbResultado.Text = "Introduce un DNI";
+            }
+
+        }
+
+    }
+}
---
+++ Tema 3/Práctica 10/Ejercicio 2/Ejercicio 2/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+
+namespace Ejercicio_2
+{
+    public partial class MainWindow : Window
+    {
+        int[,] matriz;
+        int dim;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInicializar_Click(object sender, RoutedEventArgs e)
+        {
+            dim = int.Parse(tbDimension.Text);
+            matriz = new int[dim, dim];
+            Random rnd = new Random();
+
+            for (int i = 0; i < dim; i++)
+            {
+                for (int j = 0; j < dim; j++)
+                {
+                    matriz[i, j] = rnd.Next(0, 100);
+                }
+            }
+
+            tbSalida.Text = "Matriz generada.";
+        }
+
+        private void btnImprimirMatriz_Click(object sender, RoutedEventArgs e)
+        {
+            string resultado = "";
+
+            for (int i = 0; i < dim; i++)
+            {
+                for (int j = 0; j < dim; j++)
+                {
+                    resultado += matriz[i, j] + "\t";
+                }
+                resultado += "\n";
+            }
+
+            tbSalida.Text = resultado;
+        }
+
+        private void btnImprimirTraspuesta_Click(object sender, RoutedEventArgs e)
+        {
+            string resultado = "";
+
+            for (int i = 0; i < dim; i++)
+            {
+                for (int j = 0; j < dim; j++)
+                {
+                    resultado += matriz[j, i] + "\t";
+                }
+                resultado += "\n";
+            }
+
+            tbSalida.Text = resultado;
+        }
+    }
+}
