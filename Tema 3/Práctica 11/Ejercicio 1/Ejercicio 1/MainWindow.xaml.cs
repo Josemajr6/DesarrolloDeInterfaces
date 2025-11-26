c21d074f7cdd2e8a8edcf3bf5f31c7f68c70659a
José Manuel <josemajimenezrodriguez8@gmail.com>
2025-11-26 14:37:06 +0100

    Subida de ejercicios 1 y 2 de la Práctica 11 del Tema 3

---
+++ Tema 3/Práctica 11/Ejercicio 1/Ejercicio 1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRojo_Click(object sender, RoutedEventArgs e)
+        {
+            bordeBotones.BorderBrush = Brushes.Red;
+        }
+
+        private void btnNaranja_Click(object sender, RoutedEventArgs e)
+        {
+            bordeBotones.BorderBrush = Brushes.Orange;
+        }
+
+        private void btnVerde_Click(object sender, RoutedEventArgs e)
+        {
+            bordeBotones.BorderBrush = Brushes.Green;
+        }
+    }
+}
---
+++ Tema 3/Práctica 11/Ejercicio 2/Ejercicio 2/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+  
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+            byte r = (byte)slRojo.Value;
+            byte g = (byte)slVerde.Value;
+            byte b = (byte)slAzul.Value;
+
+
+            Color colorActual = Color.FromRgb(r, g, b);
+            VentanaPrincipal.Background = new SolidColorBrush(colorActual);
+        }
+    }
+}
