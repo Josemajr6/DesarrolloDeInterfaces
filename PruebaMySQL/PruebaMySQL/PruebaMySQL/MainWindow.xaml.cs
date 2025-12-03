4f853a3d33e5119ba9de0ba7417dc1259a1e1cac
José Manuel <josemajimenezrodriguez8@gmail.com>
2025-12-03 13:30:18 +0100

    Subida del resto de la Práctica 11 y prueba con MySQL

---
+++ PruebaMySQL/PruebaMySQL/PruebaMySQL/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PruebaMySQL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string connections = "Server=localhost;Database=PruebaWPF;User=root;Password=password";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnConectar_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder resultados = new StringBuilder();
+            resultados.AppendLine("------ DATOS DE CLIENTES ------");
+
+            using (MySqlConnection connection = new MySqlConnection(connections))
+            {
+                try
+                {
+                    connection.Open();
+                    string sql = "SELECT idUsers, name, subname FROM users";
+                    using (MySqlCommand command = new MySqlCommand(sql, connection))
+                    {
+                        using (MySqlDataReader reader = command.ExecuteReader())
+                            while (reader.Read())
+                            {
+                                int id = reader.GetInt32("idUsers");
+                                string nombre = reader.GetString("name");
+                                string apellido;
+                                if (reader.IsDBNull(reader.GetOrdinal("subname")))
+                                {
+                                    apellido = string.Empty;
+                                }
+                                else
+                                {
+                                    apellido = reader.GetString("subname");
+                                }
+                                resultados.AppendLine($"ID: {id}, Nombre: {nombre}, Apellidos: {apellido}");
+                            }
+                    }
+                    if (resultados.Length <= 0)
+                    {
+                        resultados.AppendLine("No se encontraron datos");
+                    }
+                    tbResultado.Text = resultados.ToString();
+                }
+                catch (Exception ex)
+                {
+                    tbResultado.Text = $"ERROR al conectar con la BBDD{ex.Message}";
+                }
+            }
+        }
+    }
+}
---
+++ Tema 3/Práctica 11/Ejercicio 4/Ejercicio 4/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Ejercicio_4
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+  
+        private void BtnAnterior_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabControl.SelectedIndex > 0)
+            {
+                tabControl.SelectedIndex--;
+            }
+        }
+
+     
+        private void BtnSiguiente_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabControl.SelectedIndex < tabControl.Items.Count - 1)
+            {
+                tabControl.SelectedIndex++;
+            }
+        }
+
+
+        private void BtnSeleccionado_Click(object sender, RoutedEventArgs e)
+        {
+            TabItem tabActual = (TabItem) tabControl.SelectedItem;
+
+            MessageBox.Show("Has seleccionado la opción de " + tabActual.Header);
+        }
+    }
+}
